86cd8474f49b3b50298c5cec049f3912b0e0853c
ngugimuchangi <tech.curious14@gmail.com>
2022-05-20 21:30:47 +0000

    Check for negative numbers

--- printbin.c
+++ printbin.c
@@ -6,11 +6,13 @@
  */
 int print_b(va_list ap)
 {
-	unsigned int i;
+	int i;
 	long result = 0;
 	int rem, nth = 1;
 
 	i = va_arg(ap, unsigned int);
+	if (i < 0)
+		i *= -1;
 	while (i != 0)
 	{
 		rem = i % 2;
