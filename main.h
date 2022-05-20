e64b9bc534fc77844a51fed72202e8599e04031b
ngugimuchangi <tech.curious14@gmail.com>
2022-05-20 14:58:37 +0000

    Modified header file

--- main.h
+++ main.h
@@ -1,13 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
+
 int _putchar(char c);
+int _putint(int n);
 /**
  * struct spec - A new struct type holding specifier and its conversion printer
  * @sp: conversion specifier
  * @f: function pointer to function that prints the relevant data type
  */
-
 typedef struct spec
 {
 	char *sp;
@@ -16,4 +18,7 @@ typedef struct spec
 int _printf(const char *format, ...);
 int print_c(va_list ap);
 int print_s(va_list ap);
+int print_i(va_list ap);
+int print_b(va_list ap);
+
 #endif
