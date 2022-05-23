e8ecc814c83e4b5f667b44977ee6b106ed16eace
ngugimuchangi <tech.curious14@gmail.com>
2022-05-23 13:44:38 +0000

    Consolidate select function into vprintf

--- _vprintf.c
+++ _vprintf.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * selfunc - selects function to used based on specifier
+ * @op: struct to check for specifier
+ * @sp: format specifier
+ * Return: The name of the function.
+ */
+spec_t selfunc(spec_t op[], char sp)
+{
+	int i = 0;
+
+	for (i = 0; op[i].sp != NULL; i++)
+	{
+		if (sp == *(op[i].sp))
+			break;
+	}
+	return (op[i]);
+}
+/**
  * _vprintf - check for string format
  * @op: struct of functions to select
  * @format: format string;
